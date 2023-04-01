b9a487366a3e04e328a91aa20e88062ea20ce8b9
M.C <hanhchaumn@gmail.com>
2023-04-01 16:49:39 +0100

    Update to v9

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ class ShowPopoutModifyPermissions {
 	static addShowCharacterListener(popout, html, data) {
 		if (game.user.isGM) {
 			$(html).on("click", ".share-image", () => {
-				console.log("minikui", popout);
 				const relatedData = popout._related.data;
 				const actor = game.actors.get(relatedData._id);
 				this._makeCharacterVisibleToPlayers(actor);
@@ -24,18 +23,18 @@ class ShowPopoutModifyPermissions {
 
 	static _makeCharacterVisibleToPlayers(actor) {
 		if (actor) {
-			this._updatePermission(actor, ENTITY_PERMISSIONS.LIMITED);
+			this._updatePermission(actor, CONST.ENTITY_PERMISSIONS.LIMITED);
 		}
 	}
 
 	static _makeJournalVisibleToPlayers(journal) {
 		if (journal) {
-			this._updatePermission(journal, ENTITY_PERMISSIONS.OBSERVER);
+			this._updatePermission(journal, CONST.ENTITY_PERMISSIONS.OBSERVER);
 		}
 	}
 
 	static _updatePermission(object, permission) {
-		if (object.data.permission.default === ENTITY_PERMISSIONS.NONE) {
+		if (object.data.permission.default === CONST.ENTITY_PERMISSIONS.NONE) {
 			object.update(
 				{
 					permission: {
